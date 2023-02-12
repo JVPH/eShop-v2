1efbd8b6f612de63884862ab3ed24a62f929f549
JVPH <gyrospizzeria@protonmail.com>
2023-02-12 16:02:41 -0500

    feat: add rating component to display reviews as starts

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types'
+
+
+const Rating = ({ value, text, color }) => {
+  const starValues = [1, 2, 3, 4, 5]
+  return (
+    <div className="rating">
+      {starValues.map((star, index) => (
+        <span key={index}>
+          <i style={{color}}
+            className={
+              value >= star
+                ? 'fa-sharp fa-solid fa-star'
+                : value >= star - 0.5
+                  ? 'fa-sharp fa-solid fa-star-half'
+                  : 'fa-sharp fa-regular fa-star'
+            }
+          />
+        </span>
+      ))}
+      <span> {text && text}</span>
+    </div>
+  )
+}
+
+Rating.defaultProps = {
+  color: '#FFA41C'
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string
+}
+export default Rating
