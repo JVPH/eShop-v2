fd90d314c5836ba576b26101205a48e29b0a562a
JVPH <gyrospizzeria@protonmail.com>
2023-02-12 13:47:46 -0500

    setup react-bootstrap

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {  
+  "extends": "react-app",
+    "rules": {
+      "no-unexpected-multiline": "error",
+      "quotes": [
+        "error",
+        "single"
+      ],
+      "semi": [
+        "error",
+        "never"
+      ]
+  }
+}
