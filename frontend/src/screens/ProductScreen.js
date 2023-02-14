9c5e09c3ac14c4d6ff2f90a919226cc95695d3be
JVPH <gyrospizzeria@protonmail.com>
2023-02-13 20:07:02 -0500

    feat: update frontend to get the data from the backend

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
+import axios from 'axios'
 import { useParams } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { Row, Col , Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+
 
 const ProductScreen = () => {
-  let { id } = useParams()
-  const product = products.find(product => product._id === id)
+  const [product, setProduct] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`)
+
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [id])
 
   return (
     <>
