f9dfb0f2333ce84e896140f56e741a58626b4309
JVPH <gyrospizzeria@protonmail.com>
2023-02-14 19:17:20 -0500

    feat: add user, product and order models

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },  
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+}, {
+  timestamps: true
+} )
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    //the password hash should not be revealed
+    delete returnedObject.passwordHash
+  }
+})
+
+const User = mongoose.model('User', userSchema)
+
+export default User
