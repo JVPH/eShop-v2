9fb5a3b58e7fe9f1b5ba5722fbffcf8f5bbcef0f
JVPH <gyrospizzeria@protonmail.com>
2023-03-01 17:50:37 -0500

    feat: implement CheckoutSteps component in the shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import FormContainer from '../components/FormContainer'
+import CheckoutSteps from '../components/CheckoutSteps'
 import { useSelector, useDispatch } from 'react-redux'
 import { setShippingAddress } from '../features/shippingSlice'
 
@@ -25,6 +26,7 @@ const ShippingScreen = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
@@ -71,7 +73,7 @@ const ShippingScreen = () => {
           </Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button className='my-3' type='submit' variant='primary'>
           Continue
         </Button>
       </Form>
