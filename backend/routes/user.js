f47a78184dcd6b811db0525dd6b208d4e737002d
JVPH <gyrospizzeria@protonmail.com>
2023-03-10 22:43:00 -0500

    feat: add getAllUsers endpoint

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 import express from 'express'
 const userRouter = express.Router()
-import { authUser, registerUser, getUserProfile, updateUserProfile } from '../controllers/user.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, registerUser, getUserProfile, updateUserProfile, getAllUsers } from '../controllers/user.js'
+import { protect, adminOnly } from '../middleware/authMiddleware.js'
 
-userRouter.route('/').post(registerUser)
+userRouter.route('/').post(registerUser).get(protect, adminOnly, getAllUsers)
 userRouter.post('/login', authUser)
 userRouter.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
