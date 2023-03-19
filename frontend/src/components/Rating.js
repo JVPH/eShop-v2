c2334f95afef0534e092afaab7cf05af0930f84d
JVPH <gyrospizzeria@protonmail.com>
2023-03-19 16:08:05 -0500

    refactor: remove propTypes from rating component

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,6 +1,3 @@
-import PropTypes from 'prop-types'
-
-
 const Rating = ({ value, text, color }) => {
   const starValues = [1, 2, 3, 4, 5]
   return (
@@ -27,9 +24,4 @@ Rating.defaultProps = {
   color: '#FFA41C'
 }
 
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string
-}
 export default Rating
