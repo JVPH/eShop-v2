18efba4a03e0f09eb4270826d7670fb8b6ce81a6
JVPH <gyrospizzeria@protonmail.com>
2023-03-19 17:29:36 -0500

    feat: add Morgan middleware for HTTP request logging

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import path from 'path'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 import 'express-async-errors'
 import express from 'express'
 import { unknownEndpoint, errorHandler } from './middleware/errorMiddleware.js'
@@ -17,6 +18,15 @@ connectDB()
 const app = express()
 
 app.use(express.json())
+
+morgan.token('data', (req, _res) => {
+  if (Object.keys(req.body).length === 0) {
+    return '-'
+  }
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
 app.get('/', (req, res) => {
   res.send('Api is running...')
