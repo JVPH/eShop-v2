9a106055a66a0d84f87e4bfea3cecb74baf4087a
JVPH <gyrospizzeria@protonmail.com>
2023-03-21 18:12:32 -0500

    feat: wrap app with HelmetProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,16 @@ import { Provider } from 'react-redux'
 import { store } from './store'
 import './bootstrap.min.css'
 import './index.css'
+import { HelmetProvider } from 'react-helmet-async'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <Provider store={store}>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </Provider>
 )
 
