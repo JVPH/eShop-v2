e0687684110bd9d833e3650e0f32be8261fb47df
JVPH <gyrospizzeria@protonmail.com>
2024-05-30 14:29:16 -0500

    refactor: update models to use drizzle with pg
    Add reviews table

--- backend/models/product.js
+++ backend/models/product.js
@@ -1,73 +1,15 @@
-import mongoose from "mongoose"
+import { pgTable, text, uuid, timestamp, integer, numeric } from "drizzle-orm/pg-core";
 
-const reviewSchema = new mongoose.Schema({
-  name: { type: String, required: true},
-  rating: { type: Number, required: true },
-  comment: { type: String, required: true },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
-}, { timestamps: true })
+export const products = pgTable('products', {
+  id: uuid('id').defaultRandom().primaryKey(),
+  name: text('name').notNull(),
+  image: text('image').notNull(),
+  brand: text('brand').notNull(),
+  category: text('category').notNull(),
+  description: text('description').notNull(),
+  rating: integer('rating').notNull(),
+  price: numeric('price', { precision: 10, scale: 2}).notNull(),
+  countInStock: integer('countInStock').notNull(),
+  timestamp: timestamp('timestamp').notNull().defaultNow()
+});
 
-const productSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: 'User'
-  },
-  name: {
-    type: String,
-    required: true
-  },
-  image: {
-    type: String,
-    required: true    
-  },
-  brand: {
-    type: String,
-    required: true
-  },
-  category: {
-    type: String,
-    required: true    
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  reviews: [reviewSchema],
-  rating: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  numReviews: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: true,
-    default: 0
-  },
-  countInStock: {
-    type: Number,
-    required: true,
-    default: 0
-  }  
-}, {
-  timestamps: true
-})
-
-productSchema.set('toJSON', {
-  transform: (document, returnedObject) => {    
-    delete returnedObject.__v    
-  }
-})
-
-const Product = mongoose.model('Product', productSchema)
-
-export default Product
