7969252d94f13a7b1ef24947a927f1e35e76f185
JVPH <gyrospizzeria@protonmail.com>
2024-05-30 21:23:05 -0500

    refactor: import JWT_SECRET from config.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,34 +1,48 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/user.js'
+import jwt from "jsonwebtoken";
+import { users } from "../models/user.js";
+import { db } from "../config/db.js";
+import { eq } from "drizzle-orm";
+import { JWT_SECRET } from "../utils/config.js";
 
 const protect = async (req, res, next) => {
+  let token;
+  const authorization = req.get("authorization");
 
-  let token
-  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith("bearer ")) {
+    token = authorization.substring(7);
 
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    token = authorization.substring(7)
+    const decoded = jwt.verify(token, JWT_SECRET);
 
-    const decoded = jwt.verify(token, process.env.SECRET)
-    
-    req.user = await User.findById(decoded.id)
+    const resultData = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, decoded.id));
+
+    const user = resultData[0];
+
+    if (user) {
+      req.user = user;
+    } else {
+      res.status(401);
+      throw new Error("User not found");
+    }
   }
 
-  if(!token) {
-    res.status(401)
-    throw new Error('Not authorized, no token')
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
   }
 
-  next()
-}
+  next();
+};
 
 const adminOnly = (req, res, next) => {
-  if(req.user && req.user.isAdmin) {
-    next()
+  if (req.user && req.user.isAdmin) {
+    next();
   } else {
-    res.status(401)
-    throw new Error('Not authorized, Not Admin')
+    res.status(401);
+    throw new Error("Not authorized, Not Admin");
   }
-}
+};
 
-export { protect, adminOnly }
+export { protect, adminOnly };
