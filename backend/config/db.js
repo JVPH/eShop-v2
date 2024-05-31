b1635c58334dd7da3a51007b2899bbe6e3f3e710
JVPH <gyrospizzeria@protonmail.com>
2024-05-30 21:19:34 -0500

    fix: import pg package properly

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,20 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
-import { PG_HOST, PG_PORT, PG_USER, PG_PASSWORD, PG_DATABASE } from '../utils/config';
+import pg from "pg";
+const { Pool } = pg;
+import {
+  PG_HOST,
+  PG_PORT,
+  PG_USER,
+  PG_PASSWORD,
+  PG_DATABASE,
+} from "../utils/config.js";
 
 const pool = new Pool({
-      host: PG_HOST,
-      port: PG_PORT,
-      user: PG_USER,
-      password: PG_PASSWORD,
-      database: PG_DATABASE,
+  host: PG_HOST,
+  port: PG_PORT,
+  user: PG_USER,
+  password: PG_PASSWORD,
+  database: PG_DATABASE,
 });
 
 export const db = drizzle(pool);
